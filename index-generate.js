b3eef5b35d8bc7dc971382e7e044a22d1c883236
arandompieceofcheese <facepalmgamingderp@gmail.com>
2018-10-15 14:37:32 -0500

    typo

--- index-generate.js
+++ index-generate.js
@@ -4,7 +4,7 @@ const fs = require('fs-extra');
     case 'browser_action':
       if(!await fs.exists('./manifest.json')) process.exit(1, console.log("Can't find manifest, exiting..."));
       let manifest = JSON.parse(await fs.readFile('./manifest.json', 'utf8'));
-      manifest.broswer_action = {default_icon: "icon32.png", default_title: process.argv[3] || "My Extention", default_popup: "popup.html"};
+      manifest.browser_action = {default_icon: "icon32.png", default_title: process.argv[3] || "My Extention", default_popup: "popup.html"};
       await fs.writeFile('./manifest.json', JSON.stringify(manifest));
       await fs.copy(__dirname + '/defaults/icon32.png', './icon32.png');
       await fs.copy(__dirname + '/defaults/popup.html', './popup.html');
